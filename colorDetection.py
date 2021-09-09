4851e05c4e61e5f8efa1dafb2b69c28c17a1e819
jakl115 <43808301+jakl115@users.noreply.github.com>
2021-09-09 21:39:09 +0200

    colorDetection

---
+++ colorDetection.py
@@ -0,0 +1,42 @@
+import cv2
+import numpy as np
+
+path = "Resources/Kofuku.jpg"
+
+
+def empty(a):
+    pass
+
+
+trackbarName = "Trackbars"
+cv2.namedWindow(trackbarName)
+cv2.resizeWindow(trackbarName, 640, 280)
+cv2.createTrackbar("Hue min", trackbarName, 34, 179, empty)
+cv2.createTrackbar("Hue max", trackbarName, 179, 179, empty)
+cv2.createTrackbar("Sat min", trackbarName, 82, 255, empty)
+cv2.createTrackbar("Sat max", trackbarName, 161, 255, empty)
+cv2.createTrackbar("Val min", trackbarName, 216, 255, empty)
+cv2.createTrackbar("Val max", trackbarName, 255, 255, empty)
+
+
+
+while  True:
+    img = cv2.imread(path)
+
+    imgHSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    h_min = cv2.getTrackbarPos("Hue min", trackbarName)
+    h_max = cv2.getTrackbarPos("Hue max", trackbarName)
+    s_min = cv2.getTrackbarPos("Sat min", trackbarName)
+    s_max = cv2.getTrackbarPos("Sat max", trackbarName)
+    v_min = cv2.getTrackbarPos("Val min", trackbarName)
+    v_max = cv2.getTrackbarPos("Val max", trackbarName)
+
+    lower = np.array([h_min, s_min, v_min])
+    upper = np.array([h_max, s_max, v_max])
+    mask = cv2.inRange(imgHSV, lower, upper)
+
+    cv2.imshow("Original", img)
+    cv2.imshow("HSV", imgHSV)
+    cv2.imshow("Mask", mask)
+
+    cv2.waitKey(1)
