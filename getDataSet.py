1e951b468e719c6df6ebb4999fe83b578b6129d4
jakl115 <43808301+jakl115@users.noreply.github.com>
2021-09-11 18:44:02 +0200

    getDataSet fixed.

--- getDataSet.py
+++ getDataSet.py
@@ -1,8 +1,12 @@
 from icrawler.builtin import BingImageCrawler
 
-cubes = ['2x2 cube', '3x3 cube', '4x4 cube']
-cubesNo = 400
+cubeTypes = ['2x2 cube', '3x3 cube', '4x4 cube']
+cubesNo = 300
 
-for c in cubes:
+filters = dict(
+    license='creativecommons'
+)
+
+for c in cubeTypes:
     bingCrawler = BingImageCrawler(storage={'root_dir': f'tst/{c}'})
-    bingCrawler.crawl(keyword=c, filters=None, max_num=cubesNo, offset=0)
+    bingCrawler.crawl(keyword=c, filters=filters, max_num=cubesNo, offset=0)
