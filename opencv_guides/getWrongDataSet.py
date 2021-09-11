3ce20acb774fb7e20895955a5d4183f7119b9178
jakl115 <43808301+jakl115@users.noreply.github.com>
2021-09-11 18:40:52 +0200

    Files removed.

--- opencv_guides/getWrongDataSet.py
+++
@@ -1,8 +0,0 @@
-from icrawler.builtin import BingImageCrawler
-
-classes = ['human', 'tree', 'cat', 'table', 'car']
-number = 100
-
-for c in classes:
-    bingCrawler = BingImageCrawler(storage={'root_dir': f'dataset/n'})
-    bingCrawler.crawl(keyword=c, filters=None, max_num=number, offset=0)
